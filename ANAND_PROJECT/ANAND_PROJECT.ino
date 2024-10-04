65396fcb8e6bfdb79ca6c97557efbc36a5777447
Antonio Justino <jecinodaf@gmail.com>
2024-10-04 19:03:25 -0300

    Added more sensors in the code

--- ANAND_PROJECT/ANAND_PROJECT.ino
+++ ANAND_PROJECT/ANAND_PROJECT.ino
@@ -1,49 +1,75 @@
+#include <Servo.h>
+
 #define led 8
 #define sensor 7
+#define Presenca 4
+#define nivel_agua A0
+#define servo 4
 
 #define TMP_DETECT 5000
+
+Servo meuServo;
 
 int vibracoes = 0;
 float taxaVibracao = 0;
 int initialTime = 0;
 int currentTime = 0;
+int alguempresente = 0;
 
 void setup() {
   Serial.begin(9600);
   pinMode(sensor, INPUT);
   pinMode(led, OUTPUT);
+  pinMode(Presenca, INPUT);
+  pinMode(nivel_agua, INPUT);
+  meuServo.attach(servo);
 
 }
 
 void loop() {
   currentTime = millis();
-
-  if(currentTime - initialTime < TMP_DETECT){
-    if(digitalRead(sensor) == LOW){   
-      vibracoes += 1; 
-      Serial.print("Vibração detectada\n");
-    } 
-  }
+  alguempresente = digitalRead(Presenca);
   
-  else{
-    taxaVibracao = vibracoes / 5;
-
-    Serial.print("Taxa de vibraçao: ");
-    Serial.println(taxaVibracao);
+  //Se houve alguem presente
+  if(alguempresente == 1){
     
-    if(taxaVibracao >= 2){
-      digitalWrite(led, HIGH);
-      vibracoes = 0;
-      taxaVibracao = 0;
-      initialTime = currentTime;
+    //Medindo a quantidade de vibração enquanto não acabar o tempo
+    if(currentTime - initialTime < TMP_DETECT){
+      if(digitalRead(sensor) == LOW){   
+        vibracoes += 1; 
+        Serial.print("Vibração detectada\n");
+      } 
     }
+  
+    //Quando terminado o tempo, calcular a taxa e decidir
+    else{ 
+      taxaVibracao = vibracoes / 5;
+
+      Serial.print("Taxa de vibraçao: ");
+      Serial.println(taxaVibracao);
+      
+      int nivelagua = analogRead(nivel_agua);
+      map(nivelagua, 0, 1023, 0, 255);
     
-    else{
-      digitalWrite(led, LOW);
-      vibracoes = 0;
-      taxaVibracao = 0;
-      initialTime = currentTime;
+      //Se a taxa de vibração for maior que x dar descarga
+        if(nivelagua >= 128){
+          if(taxaVibracao >= 2){
+            digitalWrite(led, HIGH);
+            meuServo.write(90);
+            delay(10000);
+            vibracoes = 0;
+            taxaVibracao = 0;
+            initialTime = currentTime;
+          }
+        }
+      
+        //Se não, atualiza as variáveis
+        else{
+          digitalWrite(led, LOW);
+          vibracoes = 0;
+          taxaVibracao = 0;
+          initialTime = currentTime;
+        }
     }
   }
-
 }
--- FINAL_PROJECT/FINAL_PROJECT/FINAL_PROJECT.ino
+++ FINAL_PROJECT/FINAL_PROJECT/FINAL_PROJECT.ino
@@ -1,17 +1,23 @@
-#define Entrada1 2
-#define Entrada2 4
+#define Entrada1 12
+#define Entrada2 13
 
-#define Entrada3 12
-#define Entrada4 13
+#define Entrada3 2
+#define Entrada4 4
 
 #define UltraSonicECHO A0
 #define UltraSonicTRIGGER 5
 #define IRSensor 3
 
+#define TIME_MIN 150
+
+int ultimaalteracao = 0;
+int time_atual = 0;
 
 void setup(){
   pinMode(Entrada1, OUTPUT);
   pinMode(Entrada2, OUTPUT);
+  pinMode(Entrada3, OUTPUT);
+  pinMode(Entrada4, OUTPUT);
   pinMode(UltraSonicECHO, INPUT);
   pinMode(UltraSonicTRIGGER, OUTPUT);
   pinMode(IRSensor, INPUT);
@@ -32,12 +38,12 @@ void girarDireita(int direcao){
   
   //1 para frente, 0 para trás
   if(direcao == 1){
-    digitalWrite(Entrada1, HIGH);
-    digitalWrite(Entrada2, LOW);
+    digitalWrite(Entrada3, HIGH);
+    digitalWrite(Entrada4, LOW);
   }
   else{
-    digitalWrite(Entrada1, LOW);
-    digitalWrite(Entrada2, HIGH);
+    digitalWrite(Entrada3, LOW);
+    digitalWrite(Entrada4, HIGH);
   }
 }
 
@@ -45,44 +51,51 @@ void girarEsquerda(int direcao){
   
   //1 para frente, 0 para trás
   if(direcao == 1){
-    digitalWrite(Entrada3, LOW);
-    digitalWrite(Entrada4, HIGH);
+    digitalWrite(Entrada1, LOW);
+    digitalWrite(Entrada2, HIGH);
   }
   else{
-    digitalWrite(Entrada3, HIGH);
-    digitalWrite(Entrada4, LOW);
+    digitalWrite(Entrada1, HIGH);
+    digitalWrite(Entrada2, LOW);
   }
 }
 
 void loop(){
+
+  time_atual = millis();
   
-  if(digitalRead(IRSensor) == 1){
+    if(digitalRead(IRSensor) == 1){
     
-    digitalWrite(UltraSonicTRIGGER, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(UltraSonicTRIGGER, LOW);
-  
-    int duracao = pulseIn(UltraSonicECHO, HIGH);
-  
-    int distancia = duracao * 0.01715;
+      digitalWrite(UltraSonicTRIGGER, HIGH);
+      delayMicroseconds(10);
+      digitalWrite(UltraSonicTRIGGER, LOW);
     
-    Serial.write("Distancia:");
-    Serial.print(distancia, DEC);
-    Serial.write("\n");
-  
-    if (distancia < 60){
-      girarEsquerda(1);
-      girarDireita(1);
+      int duracao = pulseIn(UltraSonicECHO, HIGH);
+    
+      int distancia = duracao * 0.01715;
+      
+      Serial.write("Distancia:");
+      Serial.print(distancia, DEC);
+      Serial.write("\n");
+      
+      if(time_atual - ultimaalteracao >= TIME_MIN){
+        if (distancia < 60){
+          girarEsquerda(0);
+          girarDireita(1);
+          ultimaalteracao = time_atual;
+        }
+        else{
+          girarEsquerda(1);
+          girarDireita(1);
+          ultimaalteracao = time_atual;
+          
+        }
+      }
     }
+
     else{
       girarEsquerda(0);
       girarDireita(1);
-    }
-  }
-
-  else{
-    girarEsquerda(0);
-    girarDireita(1);
-    delay(500);
-  }
+      ultimaalteracao = time_atual;
+    }  
 }
