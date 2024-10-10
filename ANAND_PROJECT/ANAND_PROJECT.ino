101ac9de7b1e3024266a871a96ddb4b2f053350f
Antonio Justino <jecinodaf@gmail.com>
2024-10-10 10:52:49 -0300

    Added the sensors code

--- ANAND_PROJECT/ANAND_PROJECT.ino
+++ ANAND_PROJECT/ANAND_PROJECT.ino
@@ -1,10 +1,12 @@
+#include <Wire.h>
 #include <Servo.h>
+
 
 #define led 8
 #define sensor 7
-#define Presenca 4
-#define nivel_agua A0
-#define servo 4
+#define Presenca 12
+#define nivel_agua A1
+#define servo 5
 
 #define TMP_DETECT 5000
 
@@ -16,20 +18,59 @@ int initialTime = 0;
 int currentTime = 0;
 int alguempresente = 0;
 
+const int MPU=0x68;
+int AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+
 void setup() {
+
+  //Inicializa o MPU-6050
+  Wire.begin();
+  Wire.beginTransmission(MPU);
+  Wire.write(0x6B); 
+  Wire.write(0); 
+  Wire.endTransmission(true);
+  
   Serial.begin(9600);
   pinMode(sensor, INPUT);
   pinMode(led, OUTPUT);
   pinMode(Presenca, INPUT);
   pinMode(nivel_agua, INPUT);
   meuServo.attach(servo);
+  meuServo.write(0);
 
 }
-
+//AAAAAAAAAAAAAAAAAAAAAAAAA
 void loop() {
-  currentTime = millis();
-  alguempresente = digitalRead(Presenca);
+  delay(500);
+  Wire.beginTransmission(MPU);
+  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
   
+  //Solicita os dados do sensor
+  Wire.requestFrom(MPU,14,true);
+
+  AcX=Wire.read()<<8|Wire.read();  //0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)     
+  AcY=Wire.read()<<8|Wire.read();  //0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  AcZ=Wire.read()<<8|Wire.read();  //0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+  Tmp=Wire.read()<<8|Wire.read();  //0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+  GyX=Wire.read()<<8|Wire.read();  //0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+  GyY=Wire.read()<<8|Wire.read();  //0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+  GyZ=Wire.read()<<8|Wire.read();  //0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+
+  Serial.print("Temperatura: ");
+  Serial.println(Tmp/340.00+36.53);
+
+  Serial.print("Acelerometro: ");
+  Serial.println(AcX);
+  
+  Serial.println(digitalRead(Presenca));
+  
+  currentTime = millis();
+
+  if(alguempresente != 1){
+    alguempresente = digitalRead(Presenca);
+  }
+   
   //Se houve alguem presente
   if(alguempresente == 1){
     
@@ -37,7 +78,6 @@ void loop() {
     if(currentTime - initialTime < TMP_DETECT){
       if(digitalRead(sensor) == LOW){   
         vibracoes += 1; 
-        Serial.print("Vibração detectada\n");
       } 
     }
   
@@ -50,15 +90,22 @@ void loop() {
       
       int nivelagua = analogRead(nivel_agua);
       map(nivelagua, 0, 1023, 0, 255);
+
+      Serial.print("Nivel agua: ");
+      Serial.println(nivelagua);
     
       //Se a taxa de vibração for maior que x dar descarga
         if(nivelagua >= 128){
           if(taxaVibracao >= 2){
             digitalWrite(led, HIGH);
+            
             meuServo.write(90);
-            delay(10000);
+            delay(5000);
+            meuServo.write(0);
+            
             vibracoes = 0;
             taxaVibracao = 0;
+            alguempresente = 0;
             initialTime = currentTime;
           }
         }
@@ -68,6 +115,7 @@ void loop() {
           digitalWrite(led, LOW);
           vibracoes = 0;
           taxaVibracao = 0;
+          alguempresente = 0;
           initialTime = currentTime;
         }
     }
